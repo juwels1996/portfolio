a5f48d3c686113a88a31a794927d463c14d29612
juwels1996 <68943659+juwels1996@users.noreply.github.com>
2021-12-24 14:06:37 +0600

    Add files via upload

---
+++ lib/models/Project.dart
@@ -0,0 +1,37 @@
+class Project {
+  final String? title, description;
+
+  Project({this.title, this.description});
+}
+
+List<Project> demo_projects = [
+  Project(
+    title: "BMI calculator",
+    description:
+        "Body mass index (BMI) is a measure of body fat based on height and weight that applies to adult men and women."),
+  Project(
+    title: "Google Signin in Firebase",
+    description:
+        "Firebase Authentication makes building secure sign-ins for any platform easy. This end-to-end solution supports email and passowrd, phone auth, & mult-platform login. Monitor App Performance. Build Fast For Any Device. For Mobile or Web Apps.",
+  ),
+  Project(
+    title: "Outlook Email App Redesign - Flutter Fully Responsive Design UI",
+    description:
+        "We redesign the outlook app also make it responsive so that you can run it everywhere on your phone, tab, or web. In this flutter responsive video, we will show you the real power of flutter. Make mobile, web, and desktop app from a single codebase.",
+  ),
+  Project(
+    title: "Chat/Messaging App Light and Dark Theme - Flutter UI",
+    description:
+        "Today we gonna build messing/chat app #ui using #flutter that runs both Android and iOS devices also has a dark and light theme. We create in total 4 screens all of that support both Dark Theme and Light Theme. At first, we design a welcome screen that contains an image with a tag line also has a skip button.",
+  ),
+  Project(
+    title: "Welcome page, Login Page and Sign up page - Flutter UI",
+    description:
+        "In the first part of our complete e-commerce app, we show you how you can create a nice clean onboarding screen for your e-commerce app that can run both Andriod and iOS devices because it builds with flutter. Then on the second episode, we build a Sign in, Forgot Password screen with a custom error indicator.",
+  ),
+  Project(
+    title: "Covid-19 App - Flutter UI",
+    description:
+        "We redesign the outlook app also make it responsive so that you can run it everywhere on your phone, tab, or web. In this flutter responsive video, we will show you the real power of flutter. Make mobile, web, and desktop app from a single codebase.",
+  ),
+];
